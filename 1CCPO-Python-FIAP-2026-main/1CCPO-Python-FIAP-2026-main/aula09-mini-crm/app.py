480e0c50f3833adf83707f5ddb71c284d96544de
Carlos Eduardo Affonso <affonso.cadu@gmail.com>
2026-09-17 20:49:49 -0300

    Add files via upload

--- 1CCPO-Python-FIAP-2026-main/1CCPO-Python-FIAP-2026-main/aula09-mini-crm/app.py
+++ 1CCPO-Python-FIAP-2026-main/1CCPO-Python-FIAP-2026-main/aula09-mini-crm/app.py
@@ -1,6 +1,7 @@
 from model import model_lead
 import control
 
+
 def add_lead():
     name = input("Nome: ")
     email = input("E-mail: ")
@@ -18,15 +19,47 @@ def add_lead():
 
     print("Lead adicionado (func)")
 
+
 def list_leads():
     leads = control.read_leads()
-    print(leads)
+
+    print(f"## | {"Nome":<10} | {"E-mail":<10} | Empresa")
+    for i, lead in enumerate(leads):
+        print(f"{i:02d} | {lead["name"]:<10} | {lead["email"]:<10} | {lead["company"]}")
+
+
+def search_leads():
+    query = input("Buscar por: ").strip().lower()
+    if not query:
+        print("Consulta vazia")
+        return
+
+    # vou agora enviar a busca do usuário (query) para o control
+    # o control vai comparar a busca com a base de dados
+    # e vai retornar uma lista com o resultado
+
+    leads_found = control.read_leads_search(query)
+    print(f"## | {"Nome":<10} | {"E-mail":<10} | Empresa")
+    for i, lead in leads_found:
+        print(f"{i:02d} | {lead["name"]:<10} | {lead["email"]:<10} | {lead["company"]}")
+
+
+def export_lead():
+    path_csv = control.export_csv()
+
+    if path_csv is None:
+        print("Não foi possível exportar os leads")
+    else:
+        print(f"Exportado para {path_csv}")
+
 
 def main():
     while True:
         print("\nMini CRM de leads")
         print("[1] Adicionar lead")
         print("[2] Listar leads")
+        print("[3] Buscar nome, email, empresa")
+        print("[4] Exportar para CSV")
         print("[0] Sair do programa")
 
         opt = input("Escolha uma opção: ")
@@ -34,11 +67,16 @@ def main():
             add_lead()
         elif opt == "2":
             list_leads()
+        elif opt == "3":
+            search_leads()
+        elif opt == "4":
+            export_lead()
         elif opt == "0":
             print("Até mais..")
             break
         else:
             print("Opção inválida")
+
 
 if __name__ == "__main__":
     main()
--- 1CCPO-Python-FIAP-2026-main/1CCPO-Python-FIAP-2026-main/aula09-mini-crm/control.py
+++ 1CCPO-Python-FIAP-2026-main/1CCPO-Python-FIAP-2026-main/aula09-mini-crm/control.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import json
+import json, csv
 
 DATA_DIR = Path(__file__).resolve().parent / "data"
 DATA_DIR.mkdir(exist_ok=True)
@@ -26,3 +26,31 @@ def create_lead(lead_dict):
     leads = read_leads()
     leads.append(lead_dict)
     DB_PATH.write_text(json.dumps(leads, ensure_ascii=False, indent=2), encoding="utf-8")
+
+def read_leads_search(query):
+    leads = read_leads()
+    results = []
+
+    for i, lead in enumerate(leads):
+        txt_lead = f"{lead["name"]} {lead["email"]} {lead["company"]}".lower()
+        print(txt_lead)
+
+        if query.lower() in txt_lead:
+            results.append((i, lead))
+
+    return results
+
+def export_csv():
+    path_csv = DATA_DIR / "leads.csv"
+
+    leads = read_leads()
+
+    try:
+        with path_csv.open("w", newline="", encoding = "utf-8") as file_csv:
+            writer = csv.DictWriter(file_csv, leads[0].keys())
+            writer.writeheader()
+            for row in leads:
+                writer.writerow(row)
+        return path_csv
+    except PermissionError:
+        return None
